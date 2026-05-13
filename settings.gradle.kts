d59e25576ff49dd555029bce8bc3f8629cc0e434
wangyilei <wangyilei@tcsl.com.cn>
2026-05-13 19:29:46 +0800

    update:发布maven

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,9 +20,9 @@ pluginManagement {
         eachPlugin {
             if (requested.id.toString() == "com.arkivanov.gradle.setup") {
                 // 优先使用本地 mavenLocal 中的插件版本（支持 ohosArm64）
-                val localPlugin = file("${System.getProperty("user.home")}/.m2/repository/com/arkivanov/gradle/gradle-setup-plugin/0.0.1")
+                val localPlugin = file("${System.getProperty("user.home")}/.m2/repository/com/arkivanov/gradle/gradle-setup-plugin/0.0.1-KBA-001")
                 if (localPlugin.exists()) {
-                    useModule("com.arkivanov.gradle:gradle-setup-plugin:0.0.1")
+                    useModule("com.arkivanov.gradle:gradle-setup-plugin:0.0.1-KBA-001")
                 } else {
                     useModule("com.github.arkivanov:gradle-setup-plugin:4ae41e7b6a")
                 }
